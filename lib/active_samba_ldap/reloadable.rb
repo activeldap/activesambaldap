ee50e1ebbca51cd353089f289998e6bcecd18317
Kouhei Sutou <kou@cozmixng.org>
2007-12-12 10:43:02 +0000

    * supported ActiveSupport 2.0.x.   Reported by Francesco Malvezzi. Thanks!

--- lib/active_samba_ldap/reloadable.rb
+++ lib/active_samba_ldap/reloadable.rb
@@ -1,6 +1,8 @@
 module ActiveSambaLdap
   module Reloadable
     def self.included(base)
+      super
+      return unless Object.const_defined?(:Reloadable)
       base.class_eval do
         if ::Reloadable.const_defined?(:Deprecated)
           include ::Reloadable::Deprecated
