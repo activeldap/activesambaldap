cbff605df0cf991b8e255837228f60589a1a6323
Kouhei Sutou <kou@cozmixng.org>
2008-06-14 14:30:20 +0000

    * support LDAP benchmark. * require ActiveLdap 1.0.1.

--- rails/plugin/active_samba_ldap/init.rb
+++ rails/plugin/active_samba_ldap/init.rb
@@ -1,5 +1,15 @@
 require_library_or_gem 'active_samba_ldap'
+
 ActiveSambaLdap::Base.logger ||= RAILS_DEFAULT_LOGGER
+
+required_version = ["0", "0", "6"]
+if (ActiveLdap::VERSION.split(".") <=> required_version) < 0
+  ActiveLdap::Base.class_eval do
+    format = _("You need ActiveSambaLdap %s or later")
+    logger.error(format % required_version.join("."))
+  end
+end
+
 ldap_configuration_file = File.join(RAILS_ROOT, 'config', 'ldap.yml')
 if File.exist?(ldap_configuration_file)
   ActiveSambaLdap::Base.configurations =
@@ -15,4 +25,9 @@ end
 
 class ActionView::Base
   include ActiveLdap::Helper
+end
+
+require 'active_ldap/action_controller/ldap_benchmarking'
+class ActionController::Base
+  include ActiveLdap::ActionController::LdapBenchmarking
 end
