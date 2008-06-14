d29df10d9187841be9aaedc91657b7226bdfd5c5
Kouhei Sutou <kou@cozmixng.org>
2008-06-14 13:32:57 +0000

    * use test-unit not test-unit-ext.

--- test/run-test.rb
+++ test/run-test.rb
@@ -5,21 +5,16 @@ $VERBOSE = true
 $KCODE = 'u'
 
 top_dir = File.join(File.dirname(__FILE__), "..")
+
+$LOAD_PATH.unshift(File.join(top_dir, "test-unit", "lib"))
+require 'test/unit'
+
 # $LOAD_PATH.unshift(File.join(top_dir, "..", "ldap", "lib"))
 # $LOAD_PATH.unshift(File.join(top_dir, "..", "ldap"))
 $LOAD_PATH.unshift(File.join(top_dir, "..", "activeldap", "lib"))
 $LOAD_PATH.unshift(File.join(top_dir, "lib"))
 $LOAD_PATH.unshift(File.join(top_dir, "test"))
-$LOAD_PATH.unshift(File.join(top_dir, "test-unit-ext", "lib"))
 
-require 'test-unit-ext'
+ARGV.unshift("--priority")
 
-unless ARGV.find {|opt| /\A--(?:no-)?priority/ =~ opt}
-  ARGV << "--priority"
-end
-
-if Test::Unit::AutoRunner.respond_to?(:standalone?)
-  exit Test::Unit::AutoRunner.run($0, File.dirname($0))
-else
-  exit Test::Unit::AutoRunner.run(false, File.dirname($0))
-end
+exit Test::Unit::AutoRunner.run(true)
