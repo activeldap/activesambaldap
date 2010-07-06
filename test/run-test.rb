9a55fa0695c85206c349afb6e0d793cf19c97e18
Kouhei Sutou <kou@cozmixng.org>
2010-07-06 12:15:36 +0000

    * make test runner workable.

--- test/run-test.rb
+++ test/run-test.rb
@@ -4,7 +4,8 @@ $VERBOSE = true
 
 $KCODE = 'u'
 
-top_dir = File.join(File.dirname(__FILE__), "..")
+base_dir = File.join(File.dirname(__FILE__))
+top_dir = File.join(base_dir, "..")
 
 $LOAD_PATH.unshift(File.join(top_dir, "test-unit", "lib"))
 require 'test/unit'
@@ -13,11 +14,10 @@ require 'test/unit'
 # $LOAD_PATH.unshift(File.join(top_dir, "..", "ldap"))
 $LOAD_PATH.unshift(File.join(top_dir, "..", "activeldap", "lib"))
 $LOAD_PATH.unshift(File.join(top_dir, "lib"))
-$LOAD_PATH.unshift(File.join(top_dir, "test"))
 
 ARGV.unshift("--priority")
 
 require 'timeout'
 Test::Unit::ErrorHandler::NOT_PASS_THROUGH_EXCEPTIONS << Timeout::Error
 
-exit Test::Unit::AutoRunner.run(true, File.join(top_dir, "test"))
+exit Test::Unit::AutoRunner.run(true, base_dir)
